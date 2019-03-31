c60cf16d2fe07acb9180004d236663c11d57da8e
Belen Montes <bmontes1067@gmail.com>
2019-03-31 21:31:17 +0200

    final test

---
+++ test/spec.js
@@ -0,0 +1,8 @@
+// spec.js
+describe('Protractor Example App', function() {
+  it('should have a title', function() {
+    browser.get('https://about.google/');
+
+    expect(browser.getTitle()).toEqual('Sobre nosotros | Google');
+  });
+});
